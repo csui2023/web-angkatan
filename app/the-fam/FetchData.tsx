2a8339025c1e3708c571f9b9212edc99788d8bd7
Fazil <faziltirtana@tutanota.com>
2023-10-22 21:15:36 +0700

    fix: search major only

--- app/the-fam/FetchData.tsx
+++ app/the-fam/FetchData.tsx
@@ -3,11 +3,7 @@ import data from "../../public/data.json";
 export async function FetchData(id: string | undefined, major: string | undefined): Promise<any> {
 	return new Promise((resolve) => {
 		setTimeout(() => {
-			if (typeof id === "undefined") {
-				resolve(data)
-			} else {
-				resolve(data.filter(c => (c.id.includes(id!) && c.jurusan === major)));
-			}
+			resolve(data.filter(c => (((typeof id !== "undefined") ? c.id.includes(id) : c) && ((typeof major !== "undefined") ? c.jurusan === major : c))));
 		}, 1000);
 	});
 }
