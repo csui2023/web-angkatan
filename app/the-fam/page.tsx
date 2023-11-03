32a73eda2682b2b81e63ce7a8f8b8ac6685939ef
secona <77039267+secona@users.noreply.github.com>
2023-11-03 15:52:23 +0700

    feat: add max-width to the-fam page

--- app/the-fam/page.tsx
+++ app/the-fam/page.tsx
@@ -11,7 +11,7 @@ export default function TheFam({
     <div className="mt-36 px-6 flex flex-col items-center">
       <h1 className="text-6xl text-center mb-8">Meet Our Family!</h1>
       <SearchFam />
-      <div className="mt-16 flex flex-row flex-wrap justify-center gap-4">
+      <div className="mt-16 flex max-w-5xl flex-row flex-wrap justify-center gap-4">
         <Suspense key={searchParams.id} fallback={<div>Loading...</div>}>
           <DisplayData id={searchParams.id} major={searchParams.major} />
         </Suspense>
