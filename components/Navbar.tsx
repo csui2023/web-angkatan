6940cd3bb9d8021ce980459885c2e081df3cf1fd
secona <77039267+secona@users.noreply.github.com>
2023-11-10 19:33:32 +0700

    feat: change font of mobile navbar heading

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,13 +29,11 @@ export default function Navbar() {
   return (
     // TODO: Make Navbar responsive
     <div className="fixed top-0 left-0 w-screen h-screen invisible z-50">
-      <nav className="visible fixed inset-x-5 top-5 backdrop-blur brightness-105 bg-gradient-to-br from-red-400/10 via-blue-400/10 to-cyan-400/10 border-white border-2 border-opacity-30 shadow-xl rounded-3xl z-50 px-10 py-3 10 flex flex-row content-center justify-between items-center">
+      <nav className="visible fixed inset-x-5 top-5 backdrop-blur bg-gradient-to-br from-red-400/10 via-blue-400/10 to-cyan-400/10 border-white border-2 border-opacity-30 shadow-xl rounded-3xl z-50 px-10 py-3 10 flex flex-row content-center justify-between items-center">
         <Link href="/">
           <Image src="/logo.png" alt="" width={48} height={49} />
         </Link>
-        <div className="visible duration-700 hidden md:flex flex-col md:flex-row backdrop-blur md:backdrop-blur-0 justify-center items-center gap-4 md:gap-2">
-          <h1 className="md:hidden text-4xl mb-20">CSUI 2023</h1>
-
+        <div className="visible duration-700 hidden md:flex flex-col md:flex-row justify-center items-center gap-4 md:gap-2">
           <NavLink
             isActive={pathname === "/the-fam/"}
             href="/the-fam"
@@ -84,7 +82,7 @@ export default function Navbar() {
           open ? "bottom-0" : "-bottom-[150%]"
         } visible duration-700 fixed md:static h-screen w-screen md:w-auto z-50 md:h-auto flex md:hidden flex-col md:flex-row backdrop-blur md:backdrop-blur-0 justify-center items-center gap-4 md:gap-2`}
       >
-        <h1 className="md:hidden text-4xl mb-20">CSUI 2023</h1>
+        <h1 className="md:hidden text-4xl mb-20 font-clash-display">CSUI 2023</h1>
 
         <NavLink
           isActive={pathname === "/the-fam/"}
