8b7751b8d6bc723606b9ddecfdb7542a40b4bb1e
secona <77039267+secona@users.noreply.github.com>
2023-11-11 00:24:12 +0700

    feat: bold name in Chat

--- app/Chat.tsx
+++ app/Chat.tsx
@@ -13,7 +13,7 @@ export default function Chat(props: ChatProps) {
       <Image alt="" width={48} height={48} src={`/pfp/${props.pfp}.jpg`} className="rounded-full" />
       <div className={`${props.position === "left" ? "text-left mr-auto" : "text-right ml-auto"} max-w-3xl p-5 bg-white bg-opacity-30 backdrop-blur border-white border-2 border-solid border-opacity-30 rounded-xl`}>
         <p className="pb-2 px-3 border-b-2 border-white border-opacity-30 max-h-80 overflow-x-hidden overflow-auto ">{props.children}</p>
-        <p className="pt-2 px-3 text-sm">{props.name}</p>
+        <p className="pt-2 px-3 text-sm font-bold">{props.name}</p>
       </div>
     </div>
   );
