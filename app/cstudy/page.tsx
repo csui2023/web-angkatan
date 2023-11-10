fa90b6059fd3426d568ac65fd4cb6698e6d5547a
secona <77039267+secona@users.noreply.github.com>
2023-11-11 06:43:49 +0700

    feat: cstudy link open in new page

--- app/cstudy/page.tsx
+++ app/cstudy/page.tsx
@@ -9,6 +9,8 @@ export default function CStudy() {
       <div className="py-6 grid md:grid-cols-3 space-y-10 md:space-y-0 justify-center">
         <Link
           href="https://drive.google.com/drive/folders/1TgGbVuUF7adfoep8e9AFvtemibhoZlhK"
+          rel="noopener noreferrer"
+          target="_blank"
           className="shadow-lg hover:shadow-2xl transition p-5 w-[240px] lg:w-[320px] xl:w-[360px] h-[240px] lg:h-[320px] xl:h-[360px] bg-gradient-to-b from-purple-500 via-purple-400 to-white flex justify-center items-center rounded-2xl"
         >
           <h2 className="text-2xl lg:text-4xl text-white font-bold text-center">
@@ -17,6 +19,8 @@ export default function CStudy() {
         </Link>
         <Link
           href="https://drive.google.com/drive/folders/1TgGbVuUF7adfoep8e9AFvtemibhoZlhK"
+          rel="noopener noreferrer"
+          target="_blank"
           className="shadow-lg hover:shadow-2xl transition p-5 w-[240px] lg:w-[320px] xl:w-[360px] h-[240px] lg:h-[320px] xl:h-[360px] bg-gradient-to-b from-cyan-300 via-blue-300 to-blue-200 flex justify-center items-center rounded-2xl"
         >
           <h2 className="text-2xl lg:text-4xl text-white font-bold text-center">
@@ -25,6 +29,8 @@ export default function CStudy() {
         </Link>
         <Link
           href="https://drive.google.com/drive/folders/1TgGbVuUF7adfoep8e9AFvtemibhoZlhK"
+          rel="noopener noreferrer"
+          target="_blank"
           className="shadow-lg hover:shadow-2xl transition p-5 w-[240px] lg:w-[320px] xl:w-[360px] h-[240px] lg:h-[320px] xl:h-[360px] bg-gradient-to-b from-pink-200 via-pink-300 to-blue-200 flex justify-center items-center rounded-2xl"
         >
           <h2 className="text-2xl lg:text-4xl text-white font-bold text-center">
