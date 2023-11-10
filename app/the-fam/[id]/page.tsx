d9cef301072bfe879f5e77b6752dba3e91b47ed3
secona <77039267+secona@users.noreply.github.com>
2023-11-10 20:13:54 +0700

    fix: '\n' in profile

--- app/the-fam/[id]/page.tsx
+++ app/the-fam/[id]/page.tsx
@@ -8,6 +8,8 @@ export async function generateStaticParams() {
 }
 
 export default async function Profile({ params }: { params: { id: string } }) {
+  const current: any = data?.find((d: any) => d.id == params.id);
+
   return (
     <div className="w-full mx-4 md:mx-32 xl:mx-56 py-32">
       <div className="flex justify-start space-x-10 pt-5">
@@ -22,38 +24,16 @@ export default async function Profile({ params }: { params: { id: string } }) {
         </div>
         <div className="grow">
           <div>
-            <h2 className="font-bold text-purple-400">
-              {JSON.stringify(
-                data?.find((d: any) => d.id == params.id)?.jurusan,
-                null,
-                2
-              ).replace(/['"]+/g, "")}
-            </h2>
+            <h2 className="font-bold text-purple-400">{current.jurusan}</h2>
           </div>
           <div className="mt-5">
             <h1 className="font-bold text-3xl lg:text-4xl">
-              {JSON.stringify(
-                data?.find((d: any) => d.id == params.id)?.namaLengkap,
-                null,
-                2
-              ).replace(/['"]+/g, "")}
+              {current.namaLengkap}
             </h1>
-            <h2 className="text-2xl">
-              {JSON.stringify(
-                data?.find((d: any) => d.id == params.id)?.namaPanggilan,
-                null,
-                2
-              ).replace(/['"]+/g, "")}
-            </h2>
+            <h2 className="text-2xl">{current.namaPanggilan}</h2>
           </div>
           <div className="mt-2">
-            <h4 className="font-bold">
-              {JSON.stringify(
-                data?.find((d: any) => d.id == params.id)?.tanggalLahir,
-                null,
-                2
-              ).replace(/['"]+/g, "")}
-            </h4>
+            <h4 className="font-bold">{current.tanggalLahir}</h4>
           </div>
         </div>
       </div>
@@ -63,12 +43,8 @@ export default async function Profile({ params }: { params: { id: string } }) {
             <h1 className="font-bold text-2xl">Description</h1>
           </div>
           <div className="pt-2">
-            <p className="text-lg text-gray-600 text-justify">
-              {JSON.stringify(
-                data?.find((d: any) => d.id == params.id)?.deskripsi,
-                null,
-                2
-              ).replace(/['"]+/g, "")}
+            <p className="text-lg text-gray-600 text-justify whitespace-pre-line">
+              {current.deskripsi}
             </p>
           </div>
         </div>
@@ -78,67 +54,29 @@ export default async function Profile({ params }: { params: { id: string } }) {
           </div>
           <div className="text-lg">
             <Link
-              href={`https://www.instagram.com/${JSON.stringify(
-                data?.find((d: any) => d.id == params.id)?.instagram,
-                null,
-                2
-              ).replace(/['"]+/g, "")}/`}
+              href={`https://www.instagram.com/${current.instagram}`}
               className="flex items-center space-x-2"
             >
               <FaInstagram className="min-w-[18px]" />
-              <h2>
-                {JSON.stringify(
-                  data?.find((d: any) => d.id == params.id)?.instagram,
-                  null,
-                  2
-                ).replace(/['"]+/g, "")}
-              </h2>
+              <h2>{current.instagram}</h2>
             </Link>
             <Link
-              href={JSON.stringify(
-                data?.find((d: any) => d.id == params.id)?.linkedin,
-                null,
-                2
-              ).replace(/['"]+/g, "")}
+              href={current.linkedin}
               className="flex items-center space-x-2"
             >
               <FaLinkedin className="min-w-[18px]" />
-              <h2>
-                {JSON.stringify(
-                  data?.find((d: any) => d.id == params.id)?.linkedin,
-                  null,
-                  2
-                )
-                  .replace(/['"]+/g, "")
-                  .slice(24)}
-              </h2>
+              <h2>{current.linkedin.slice(24)}</h2>
             </Link>
             <Link
-              href={`https://www.twitter.com/${JSON.stringify(
-                data?.find((d: any) => d.id == params.id)?.twitter,
-                null,
-                2
-              ).replace(/['"]+/g, "")}/`}
+              href={`https://www.twitter.com/${current.twitter}/`}
               className="flex items-center space-x-2"
             >
               <FaTwitter className="min-w-[18px]" />
-              <h2>
-                {JSON.stringify(
-                  data?.find((d: any) => d.id == params.id)?.twitter,
-                  null,
-                  2
-                ).replace(/['"]+/g, "")}
-              </h2>
+              <h2>{current.twitter}</h2>
             </Link>
             <div className="flex items-center space-x-2">
               <FaLine className="min-w-[18px]" />
-              <h2>
-                {JSON.stringify(
-                  data?.find((d: any) => d.id == params.id)?.line,
-                  null,
-                  2
-                ).replace(/['"]+/g, "")}
-              </h2>
+              <h2>{current.line}</h2>
             </div>
           </div>
         </div>
@@ -147,12 +85,8 @@ export default async function Profile({ params }: { params: { id: string } }) {
             <h1 className="font-bold text-2xl">Message for GAUNG 2023</h1>
           </div>
           <div className="pt-2">
-            <p className="text-lg text-gray-600 text-justify">
-              {JSON.stringify(
-                data?.find((d: any) => d.id == params.id)?.message,
-                null,
-                2
-              ).replace(/['"]+/g, "")}
+            <p className="text-lg text-gray-600 text-justify whitespace-pre-line">
+              {current.message}
             </p>
           </div>
         </div>
@@ -162,11 +96,11 @@ export default async function Profile({ params }: { params: { id: string } }) {
           </div>
           <div>
             <ul className="list-disc pl-5">
-              {data
-                ?.find((d: any) => d.id == params.id)
-                ?.interests.map((d: any, i: number) => (
-                  <li className="text-lg text-gray-600">{d}</li>
-                ))}
+              {current?.interests.map((d: any, i: number) => (
+                <li key={i} className="text-lg text-gray-600">
+                  {d}
+                </li>
+              ))}
             </ul>
           </div>
         </div>
